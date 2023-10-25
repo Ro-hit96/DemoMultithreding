c28e4dfa1b5fdb6f20267fcef96e20a777b5daf5
StartlordR <rohitsurvase4363@gmail.com>
2023-10-25 21:27:28 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DemoMultithreding
+{
+    public class Program
+    {
+        public void First()
+        {
+            for(int i = 1; i <=5; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(2000);
+            }
+        }
+        public void Second()
+        {
+            for(int i = 6; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(2000);
+            }
+        }
+    }
+    public class MultitherdingMM
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            Thread t1 = new Thread(new ThreadStart(program.First));
+            Thread t2=new Thread(new ThreadStart(program.Second));
+            t2.Priority = ThreadPriority.Highest;
+            t1.Priority = ThreadPriority.AboveNormal;
+            t1.Start();
+          //  t1.Join();
+            t1.Join(5000);
+            t2.Start();
+        }
+    }
+}
